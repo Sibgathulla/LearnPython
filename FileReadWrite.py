228ec5236113c58999d61dd3daaec5678a0b9981
Sibgathulla <sibgath.s@gmail.com>
2018-10-12 15:00:33 -0700

    Display list of users
    Tested

--- FileReadWrite.py
+++ FileReadWrite.py
@@ -25,7 +25,7 @@ def CreateTable(tableName,columns):
     fileExtension='txt'
     fileName=tableName+'.'+fileExtension
     if(tableName!='' and not os.path.exists(fileName)):
-        fileObj=open('DbFile.txt','w')
+        fileObj=open(fileName,'w')
         #fileObj.write(tableName+'\n')
         print('File created and ready to write..')
         #fileObj.writeline('='*20)
@@ -37,16 +37,18 @@ def CreateTable(tableName,columns):
         if rowColumn != '':
             rowColumn=rowColumn+','
         rowColumn=rowColumn+column
-    fileObj.write(rowColumn)
+    fileObj.write(rowColumn+'\n')
 
     fileObj.close()
 
-    return true
+    return True
 
 def GetAllColumns(tableName):
-    fileObj=open('DbFile.txt','r')
+    fileExtension='txt'
+    fileName=tableName+'.'+fileExtension
+    fileObj=open(fileName,'r')
     columns = fileObj.readline()
-    lsColumns=colums.spilt(',')
+    lsColumns=columns.split(',')
     return lsColumns
 
 def CheckAndCreateSchema(tableName):
@@ -55,10 +57,10 @@ def CheckAndCreateSchema(tableName):
         return
     if(not IsExists(tableName)):
         columns=[]
-        print('Please provide columns, enter 0000 when done..!') 
+        print('Please provide columns, enter 0x0x0 when done..!') 
         while(True):
             columnName=input('Column Name: ')
-            if(columnName=='0000')
+            if(columnName=='0x0x0'):
                 break;
             columns.append(columnName)
             
@@ -68,17 +70,38 @@ def CheckAndCreateSchema(tableName):
         columns=GetAllColumns(tableName)
         columnVal=[]
         for column in columns:
-            val=input(column+': ')
+            val=input(column.strip()+': ')
             columnVal.append(val)
-        if(CreateTable(tableName,columns)):
+        if(CreateTable(tableName,columnVal)):
             print('Data added successfully..!')
                 
             
-        
-        
+def DisplayAllUsers(tableName):
+    fileExtension='txt'
+    fileName=tableName+'.'+fileExtension
+    fileObj=open(fileName,'r')
+    isheader=True
+    allUsers=''
+    for columnsRow in fileObj:
+        if isheader:
+            allColumns=columnsRow.split(',')
+            for column in allColumns:
+                allUsers = allUsers + column.rjust(5+len(column),' ')
+            allUsers=allUsers+'\n'+'='*50
+            isheader=False
+        else:
+            allColumns=columnsRow.strip().split(',')
+            allUsers=allUsers+'\n'
+            for column in allColumns:
+                allUsers = allUsers + column.ljust(10,' ')
+                #allUsers=allUsers+'\n'+'='*50
+            
+    print(allUsers)
+    fileObj.close()
     
 def main():
     tableName=input('Enter table Name: ')
     CheckAndCreateSchema(tableName)
+    DisplayAllUsers(tableName)
 
 main()
